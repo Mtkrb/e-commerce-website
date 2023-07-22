b6385abcbe0a8abd67ca772191c6f29e44a730eb
Bektemir Abilov <younghearth@gmail.com>
2023-07-22 13:03:15 +0600

    mobile nav added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+"use strict"
+
+//prod page product quantity picker
 $(document).ready(function () {
   $(".product__quantity-input-minus").on("click", function (e) {
     let input = $(e.target).closest(".product__quantity-input").find("input")
@@ -8,3 +11,20 @@ $(document).ready(function () {
     input[0]["stepUp"]()
   })
 })
+
+/// mobile-nav-toggle
+let burgerMenu = document.querySelectorAll(".header__burger-menu")
+let mobileNav = document.querySelectorAll(".mobile-nav")
+let mobileNavCloseIcon = document.querySelectorAll(".mobile-nav .close-icon")
+for (let i = 0; i < burgerMenu.length; i++) {
+  burgerMenu[i].addEventListener("click", function () {
+    mobileNav[0].classList.toggle("d-none")
+    mobileNav[0].classList.toggle("active")
+  })
+}
+for (let i = 0; i < mobileNavCloseIcon.length; i++) {
+  mobileNavCloseIcon[i].addEventListener("click", function () {
+    mobileNav[0].classList.toggle("d-none")
+    mobileNav[0].classList.toggle("active")
+  })
+}
